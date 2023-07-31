3e854fa4026e57646db8ac45fc0e9dc23734985f
unktir <valera47304@gmail.com>
2023-07-31 22:41:52 +0500

    fix((mainfooter)): rewrite urls

--- src/widgets/MainFooter/vars/index.js
+++ src/widgets/MainFooter/vars/index.js
@@ -12,18 +12,18 @@ export const developers = [
         name: 'kurasikirwa',
         position: 'Главный Backend-разработчик',
         urls: {
-            gitHub: 'https://github.com/LeylinV',
-            tg: 'https://t.me/LeylinV',
-            gitLab: 'https://gitlab.com/LeylinV'
+            gitHub: 'https://github.com/kurasikirwa',
+            tg: 'https://t.me/kurasikirwa',
+            gitLab: 'https://gitlab.com/kurasikirwa'
         }
     },
     {
         name: 'unktir',
         position: 'FullStack-разработчик',
         urls: {
-            gitHub: 'https://github.com/LeylinV',
-            tg: 'https://t.me/LeylinV',
-            gitLab: 'https://gitlab.com/LeylinV'
+            gitHub: 'https://github.com/unktir',
+            tg: 'https://t.me/unktir',
+            gitLab: 'https://gitlab.com/unktir'
         }
     }
 ]
