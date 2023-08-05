901659c50326ffb00a4072cf20fba8c210acd64a
unktir <valera47304@gmail.com>
2023-08-05 16:41:43 +0500

    Revert "feat(): create a custom hook useSticky for StoreHeader. Fix warnings with keys in Category and ProductCard"
    This reverts commit 194f8ea523b3a65f522c2b35cd35b2755baa4262.

--- src/entities/ProductCard/ui/index.jsx
+++ src/entities/ProductCard/ui/index.jsx
@@ -15,14 +15,16 @@ const ProductCard = (
             <Card.Body className={'d-flex flex-column justify-content-between'}>
                 <div>
                     <Card.Title>{product.title}</Card.Title>
-                    <Card.Text className='fs-6 text-secondary'>
-                        {
-                            Boolean(product?.description)
-                                ?
-                                product.description
-                                :
-                                connectIngredients(product?.ingredients)
-                        }
+                    <Card.Text>
+                        <div className='fs-6 text-secondary'>
+                            {
+                                Boolean(product?.description)
+                                    ?
+                                    product.description
+                                    :
+                                    connectIngredients(product?.ingredients)
+                            }
+                        </div>
                     </Card.Text>
                 </div>
                 <Row className={'justify-content-between pt-4'}>
--- src/widgets/StoreHeader/ui/index.jsx
+++ src/widgets/StoreHeader/ui/index.jsx
@@ -3,28 +3,24 @@ import {Container, Row, Col} from "react-bootstrap";
 import {CustomNavbar} from "shared/ui/navbar";
 import {BasketButton} from "features/BasketButton";
 
-import {getAnchors} from "../lib/helpers/getAnchors";
+import {getAnchors} from "../lib/getAnchors";
 
 import {categories} from "../vars";
-import {changeKeys} from "../lib/helpers/changeKeys";
+import {changeKeys} from "../lib/changeKeys";
 import {useEffect, useState} from "react";
-import {useSticky} from "../lib/hooks/useSticky";
 
 const StoreHeader = () => {
     const [categoriesWithAnchors, setCategoriesWithAnchors] = useState([])
-    const [ref, isSticky] = useSticky()
     useEffect(()=>{
         setCategoriesWithAnchors(changeKeys(getAnchors(categories, 'value'), 'value'))
     },[])
     return (
-        <div ref={ref} className={`position-sticky top-0 z-1 py-2 bg-white ${isSticky && 'shadow'}`}>
-            <Container>
-                <Row className={'justify-content-between'}>
-                    <Col xs="auto"><CustomNavbar navs={categoriesWithAnchors}/></Col>
-                    <Col xs="auto"><BasketButton/></Col>
-                </Row>
-            </Container>
-        </div>
+        <Container>
+            <Row className={'justify-content-between'}>
+                <Col xs="auto"><CustomNavbar navs={categoriesWithAnchors}/></Col>
+                <Col xs="auto"><BasketButton/></Col>
+            </Row>
+        </Container>
     );
 };
 
