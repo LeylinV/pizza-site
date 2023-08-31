fc9067add0e46da36aabaa5bad1049c3e6570edc
unktir <valera47304@gmail.com>
2023-08-31 16:34:51 +0500

    refactor(config/build): divide plugins into general and dev array

--- config/build/buildPlugins.js
+++ config/build/buildPlugins.js
@@ -2,16 +2,23 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
-export function buildPlugins({ paths }) {
-    return [
+export function buildPlugins({ paths, isDev }) {
+    const isProd = !isDev;
+    const plugins = [
         new HtmlWebpackPlugin({
-            title: 'Pet Pizza',
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'styles/[name].[contenthash:8].css',
-            chunkFilename: 'styles/[name].[contenthash:8].css',
-        }),
     ];
+
+    if (isProd) {
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'styles/[name].[contenthash:8].css',
+                chunkFilename: 'styles/[name].[contenthash:8].css',
+            }),
+        );
+    }
+
+    return plugins;
 }
